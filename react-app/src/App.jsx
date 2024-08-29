e94816153887b90448e8a962d9720b960257f58f
shubham <shubhamsinghmor2312@gmail.com>
2024-08-29 23:59:13 +0530

    changes done

---
+++ react-app/src/App.jsx
@@ -0,0 +1,15 @@
+// import "./App.css"
+import Title from "./title.jsx"
+import Space from './space.jsx'
+
+function App() {
+  return (
+    <div>
+      <Space />
+    </div>
+    
+  
+  )
+}
+
+export default App
---
+++ react-app/src/title.jsx
@@ -0,0 +1,4 @@
+function Title(){
+    return <h5>Richest man in the world</h5>
+  }
+  export default Title
