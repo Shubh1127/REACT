22a9b2cfa7bbcc43c2af82dd7d8540ac26bff337
shubham <shubhamsinghmor2312@gmail.com>
2024-09-24 21:27:08 +0530

    changes done

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -6,13 +6,14 @@ import ProductTab from "./productTab.jsx"
 import Button from "./button.jsx"
 import Form from "./form.jsx"
 import Contact from "./Contact.jsx"
+import  Joke  from "../../react-forms/react-forms/src/joke.jsx"
 
 function App() {
   return (
     <div>
-      <Space/>
-      {/* <Form/> */}
-      {/* <Contact/> */}
+      {/* <Space/> */}
+      {/* <Joke/> */}
+      <Contact/>
     </div>
   )
 }
